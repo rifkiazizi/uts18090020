033d04974b97a175a86a2fffb64d503465edf58a
rifkiazizi <47319509+rifkiazizi@users.noreply.github.com>
2021-04-21 00:11:00 +0700

    utst

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,24 +47,7 @@
           </div>
       </div>
     </section>
-
-    <!-- about section start -->
-    <section class="about" id="about">
-        <div class="max-width">
-            <h2 class="title">About me</h2>
-            <div class="about-content">
-                <div class="column left">
-                    <img src="images/ooo.JPG" alt="">
-                </div>
-                <div class="column right">
-                    <div class="text">Saya Ilham Hanif Pamulia dan NIM Ku :  <span class="typing-2"></span></div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ut voluptatum eveniet doloremque autem excepturi eaque, sit laboriosam voluptatem nisi delectus. Facere explicabo hic minus accusamus alias fuga nihil dolorum quae. Explicabo illo unde, odio consequatur ipsam possimus veritatis, placeat, ab molestiae velit inventore exercitationem consequuntur blanditiis omnis beatae. Dolor iste excepturi ratione soluta quas culpa voluptatum repudiandae harum non.</p>
-                    <a href="#">Download CV</a>
-                </div>
-            </div>
-        </div>
-    </section>
-
+    
     <!-- services section start -->
     <section class="services" id="services">
         <div class="max-width">
